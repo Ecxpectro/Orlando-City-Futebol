6035cc1d08e18c33416635ed883609e3306a363d
Ecxpectro <henriquescunha12345@gmail.com>
2022-08-09 08:31:54 -0300

    estilizando versão mobile

--- public/js/efeitos.js
+++ public/js/efeitos.js
@@ -1,25 +1,34 @@
-      $(document).ready(function () {
-        $("#logotipo")
-          .on("mouseover", function () {
-            $("#banner h1").addClass("efeito");
-          })
-          .on("mouseout", function () {
-            $("#banner h1").removeClass("efeito");
-          });
-        $("#input-search")
-          .on("focus", function () {
-            $("li.search").addClass("ativo");
-          })
-          .on("blur", function () {
-            $("li.search").removeClass("ativo");
-          });
-        $(".thumbnails").owlCarousel({
-          loop: true,
-          margin: 10,
-          nav: true,
-          navText: ["Anterior", "Proximo"],
-          responsive: {
-           items: 4        
-          }
-        });
-      });
+$(document).ready(function () {
+  $("#logotipo")
+    .on("mouseover", function () {
+      $("#banner h1").addClass("efeito");
+    })
+    .on("mouseout", function () {
+      $("#banner h1").removeClass("efeito");
+    });
+  $("#input-search")
+    .on("focus", function () {
+      $("li.search").addClass("ativo");
+    })
+    .on("blur", function () {
+      $("li.search").removeClass("ativo");
+    });
+  $(".thumbnails").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["Anterior", "Proximo"],
+    responsive: {
+      items: 4,     
+    },
+  });
+  var owl = $('.owl-carousel');
+  owl.owlCarousel();  
+ 
+  $('#btn-news-next').click(function() {
+    owl.trigger('next.owl');
+  });
+      $('#btn-news-prev').click(function() {
+        owl.trigger('prev.owl');
+    });
+});
